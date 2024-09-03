179e65e3a248866630bb33e671ad0185ed31bb02
lidiamiceli <miceli.lidia1996@gmail.com>
2024-09-03 16:26:32 +0200

    feat: add Select, Input, costants and mixins

---
+++ src/constants/common.ts
@@ -0,0 +1,34 @@
+export const listaGeneri = [
+    {
+      label: "Horror",
+      value: "horror",
+    },
+    {
+      label: "Fantasy",
+      value: "fantasy",
+    },
+    {
+      label: "Avventura",
+      value: "avventura",
+    },
+    {
+      label: "Romanzo",
+      value: "romanzo",
+    },
+    {
+      label: "Fantascienza",
+      value: "fantascienza",
+    },
+    {
+      label: "Narrativa",
+      value: "narrativa",
+    },
+    {
+      label: "Saggistica",
+      value: "saggistica",
+    },
+    {
+      label: "Umoristico",
+      value: "umoristico",
+    },
+  ];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,20 +2,62 @@ import Head from "next/head";
 import style from "@/styles/Home.module.css";
 import Header from "@/components/Molecules/Header/Header";
 import WindowBox from "@/components/Organism/WindowBox/WindowBox";
+import InputBox from "@/components/Molecules/InputBox/InputBox";
+import SelectBox from "@/components/Molecules/SelectBox/SelectBox";
+import { useState } from "react";
+import { listaGeneri } from "@/constants/common";
+import Button from "@/components/Atom/Button/Button";
 
 export default function Home() {
+  const [titolo, setTitolo] = useState("");
+  const [autore, setAutore] = useState("");
+  const [genere, setGenere] = useState("");
+
+  const handleGenerate = () => {
+    console.log({ titolo, autore, genere });
+  };
+
   return (
     <>
       <Head>
-        <title>AI APP</title>
-        <meta name="description" content="AI based app to generate stories" />
+        <title>AI Book Generator</title>
+        <meta name="description" content="AI based app to generate book concepts" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={style.main}>
-        <Header title="AI APP" />
+        <Header title="AI Book Generator" />
         <div className={style.content}>
-          <WindowBox title="Projects" />
+          <WindowBox title="Book Parameters">
+            <div className={style.container}>
+              <InputBox
+                label="Titolo del Libro:"
+                value={titolo}
+                setValue={setTitolo}
+              />
+              <InputBox
+                label="Nome Autore:"
+                value={autore}
+                setValue={setAutore}
+              />
+            </div>
+            <div className={style.container}>
+              <SelectBox
+                label="Genere:"
+                list={listaGeneri}
+                setAction={setGenere}
+              />
+            </div>
+            <Button
+              label="Genera"
+              onClick={handleGenerate}
+              disabled={
+                titolo.trim().length <= 0 ||
+                autore.trim().length <= 0 ||
+                genere.trim().length <= 0
+              }
+            />
+          </WindowBox>
         </div>
       </main>
     </>
